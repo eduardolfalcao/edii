1bd82a8af6601d2971e50016b56c6d8951d2930d
Eduardo Falcão <eduardo@dca.ufrn.br>
2021-07-30 19:24:06 -0300

    Removendo funcoes de navegacao em grafos

--- src/grafos/grafolistaadjexercicio.h
+++ src/grafos/grafolistaadjexercicio.h
@@ -21,15 +21,6 @@ private:
         //IMPLEMENTAR
     }
 
-    /**
-    * O argumento indicesVerticesVisitados serve para controlar quais
-    * vertices já foram visitados.
-    * Lembrando que DFS é uma função recursiva.
-    **/
-    void dfs(string rotuloVOrigem, bool* indicesVerticesVisitados) {
-        //IMPLEMENTAR
-    }
-
 public:
     /**
     * Lembrem-se:
@@ -73,74 +64,6 @@ public:
     * conectados (vizinhos).
     **/
     bool saoConectados(string rotuloVOrigem, string rotuloVDestino) {
-        //IMPLEMENTAR
-    }
-
-    /**
-    * Verifica se há algum caminho entre vértice rotuloVOrigem e 
-    * vértice rotuloVDestino.
-    * A melhor forma de fazer isto é reusando a função dfs.
-    **/
-    bool haCaminho(string rotuloVOrigem, string rotuloVDestino) {
-        //IMPLEMENTAR
-    }
-
-    /**
-    * Muda os rótulos do vértices dos diferentes componentes para
-    * nomes que representam cores.
-    * Dica: procura componentes partindo do vértice v0 ou v1, em ordem
-    * crescente (mas você pode usar outra ordem se desejar). 
-    * Retorna a quantidade de componentes.
-    * A melhor forma de fazer isto é reusando a função dfs.
-    **/
-    int colorir() {
-        //IMPLEMENTAR
-    }
-
-    /**
-    * Usa a abordagem de navegação BFS para listar as distâncias
-    * entre o vértice rotuloVOrigem e cada um dos demais vértices.
-    * A função BFS consegue listar a menor distância entre um vértice
-    * e os demais se o grafo for NÃO-PONDERADO.
-    * Retorna um vetor de inteiros com a quantidade de arestas 
-    * (distância) e o vértice rotuloVOrigem e cada um dos demais vértices.
-    * Não é uma função recursiva. 
-    * É necessário utilizar a ED fila.
-    **/
-    int* bfs(string rotuloVOrigem) {
-        //IMPLEMENTAR
-    }
-
-    /**
-    * Usamos o BellmanFord para encontrar menor caminho
-    * em grafos com arestas com pesos negativos.
-    * O BellmanFord consegue detectar ciclos negativos
-    * e atribuir o valor NEG_INF (constante definida nesse arquivo)
-    * aos vértices que pertençam ao ciclo.
-    * POS_INF deve ser atribuído aos vértices inalcançáveis.
-    * O aspecto negativo é sua complexidade de tempo: O(V*E).
-    * Isto acontece pois, como possui arestas negativas, cada vértice 
-    * do grafo precisa ser processado V vezes.   
-    * Pseudo-código: https://github.com/eduardolfalcao/edii/blob/master/conteudos/Grafos.md#bellman-ford
-    **/
-    int* bellmanFord(string rotuloVOrigem) {
-        //IMPLEMENTAR
-    }
-
-    /**
-    * Usamos o Dijkstra para encontrar menor caminho
-    * em grafos sem arestas com pesos negativos.
-    * O Dijkstra retorna respostas incorretas caso o grafo
-    * possua arestas negativas, e portanto não consegue
-    * detectar ciclos negativos. Este é o aspecto negativo.
-    * POS_INF deve ser atribuído aos vértices inalcançáveis.
-    * O aspecto positivo é sua complexidade de tempo: O(V+E).
-    * Isto acontece pois, como o grafo não possui arestas negativas, 
-    * cada vértice do grafo precisa ser processado apenas 1 vez.
-    * Pseudo-código: https://github.com/eduardolfalcao/edii/blob/master/conteudos/Grafos.md#dijkstra
-    * Ilustração: https://docs.google.com/drawings/d/1NmkJPHpcg8uVcDZ24FQiYs3uHR5n-rdm1AZwD74WiMY/edit?usp=sharing
-    **/
-    int* dijkstra(string rotuloVOrigem) {
         //IMPLEMENTAR
     }
 
